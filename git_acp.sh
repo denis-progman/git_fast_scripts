5e11eec5a7447719cf7cedc33b2b08039b560fad
denis-pogman <denis.progman@gmail.com>
2023-05-05 16:12:44 -0400

    added shor logs sowing

--- git_acp.sh
+++ git_acp.sh
@@ -5,11 +5,16 @@ git commit -am "${1}"
 git push
 
 current_brench=$(git branch --show-current) 
+git status
+echo "Work with $current_brench is compliteted."
+
 if [ ! -z $2 ] 
 then
     git checkout $2
     git merge $current_brench
     git push
-    
+
     git checkout $current_brench
+    git status
+    echo "Work with $2 is compliteted. You've swiched back to $current_brench"
 fi
